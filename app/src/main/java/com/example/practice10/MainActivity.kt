9c11674ad5dce6f05010d1a743b8f1fcdf4b2cc6
Daniil Golandzia <solovevdaniil01@gmail.com>
2022-11-01 16:31:41 +0300

    Commit adding Ex2
    Signed-off-by: user07 <user07>

--- app/src/main/java/com/example/practice10/MainActivity.kt
+++ app/src/main/java/com/example/practice10/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.linerlayout)
+        setTitle("LinearLayout")
     }
 
     fun button1Click(viewOfButton1: View){
@@ -22,5 +25,29 @@ class MainActivity : AppCompatActivity() {
         var button2: Button = findViewById(R.id.button2)
         button1.visibility = View.VISIBLE
         button2.visibility = View.INVISIBLE
+    }
+    fun orientbutton1(view: View){
+        val textOrient:TextView = findViewById(R.id.textView2)
+        textOrient.text = "Вертикальная"
+    }
+    fun gravitybutton2(view: View){
+        val textOrient:TextView = findViewById(R.id.textView4)
+        textOrient.text = "Левая"
+    }
+    fun orientbutton3(view: View){
+        val textOrient:TextView = findViewById(R.id.textView2)
+        textOrient.text = "Горизональная"
+    }
+    fun gravitybutton4(view: View){
+        val textOrient:TextView = findViewById(R.id.textView4)
+        textOrient.text = "Центр"
+    }
+    fun orientbutton5(view: View) {
+        val textOrient: TextView = findViewById(R.id.textView2)
+        textOrient.text = "Вертикальная"
+    }
+    fun gravitybutton6(view: View){
+        val textOrient:TextView = findViewById(R.id.textView4)
+        textOrient.text = "Правая"
     }
 }
